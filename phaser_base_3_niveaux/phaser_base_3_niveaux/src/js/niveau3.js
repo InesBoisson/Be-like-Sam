eeb5175420c2079053e9a4a83a3439fb86d96028
Alizée Marti <alizee.marti@epfedu.fr>
2025-03-10 17:22:30 +0100

    update du niveau 3 j'ai commencer à implémenter la map dans le niveau

--- phaser_base_3_niveaux/phaser_base_3_niveaux/src/js/niveau3.js
+++ phaser_base_3_niveaux/phaser_base_3_niveaux/src/js/niveau3.js
@@ -6,13 +6,59 @@ export default class niveau3 extends Phaser.Scene {
       key: "niveau3" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
-  preload() {}
+  preload() {
+// chargement tuiles de jeu
+this.load.image("Phaser_tuilesdejeu1", "src/assets/tavern-decoNEW.png");
+this.load.image("Phaser_tuilesdejeu2", "src/assets/tavern-furnitureNEW.png");
+this.load.image("Phaser_tuilesdejeu3", "src/assets/TopDownHouse_FloorsAndWallsNEW.png");
+
+
+// chargement de la carte
+this.load.tilemapTiledJSON("carte", "src/assets/jeu_N3.json");  
+}
 
   create() {
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
+// Chargement de la carte
+const carteDuNiveau = this.add.tilemap("carte");
+
+// Chargement des jeux de tuiles
+const tileset1 = carteDuNiveau.addTilesetImage("tuiles_de_jeu1", "Phaser_tuilesdejeu1");
+const tileset2 = carteDuNiveau.addTilesetImage("tuiles_de_jeu2", "Phaser_tuilesdejeu2");
+const tileset3 = carteDuNiveau.addTilesetImage("tuiles_de_jeu3", "Phaser_tuilesdejeu3");
+
+// Tableau des tilesets pour les couches de la carte
+const tilesets = [tileset1, tileset2, tileset3];
+
+// chargement du calque BG
+const BG = carteDuNiveau.createLayer(
+  "calque_background",
+  tileset1,tileset2,tileset3
+);
+
+// chargement du calque FG1
+const FG1 = carteDuNiveau.createLayer(
+  "calque_frontground1",
+  tileset,tileset2,tileset3
+);
+
+// chargement du calque FG2
+const FG2 = carteDuNiveau.createLayer(
+  "calque_frontground2",
+  tileset1,tileset2,tileset3
+); 
+// définition des tuiles de plateformes qui sont solides
+// utilisation de la propriété estSolide
+calque_background.setCollisionByProperty({ estSolide: true }); 
+// ajout d'une collision entre le joueur et le calque plateformes
+this.physics.add.collider(player, calque_background);
+
+// redimentionnement du monde avec les dimensions calculées via tiled
+this.physics.world.setBounds(0, 0, 3200, 640);
+//  ajout du champs de la caméra de taille identique à celle du monde
+this.cameras.main.setBounds(0, 0, 3200, 640);
+// ancrage de la caméra sur le joueur
+this.cameras.main.startFollow(player);  
+
     // ajout d'un texte distintcif  du niveau
     this.add.text(400, 100, "Vous êtes dans le niveau 3", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
