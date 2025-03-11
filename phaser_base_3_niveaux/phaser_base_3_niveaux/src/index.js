336126c38758768fe400d8c235e5acdc3286609e
Inès Boisson <ines.boisson@epfedu.fr>
2025-03-11 12:03:18 +0100

    changements

--- phaser_base_3_niveaux/phaser_base_3_niveaux/src/index.js
+++ phaser_base_3_niveaux/phaser_base_3_niveaux/src/index.js
@@ -5,6 +5,7 @@ import selection from "/src/js/selection.js";
 import niveau1 from "/src/js/niveau1.js";
 import niveau2 from "/src/js/niveau2.js";
 import niveau3 from "/src/js/niveau3.js";
+import dialogue from '/src/js/dialogue.js'; // Adjust the path as necessary
 
 // configuration générale du jeu
 var config = {
@@ -27,7 +28,7 @@ var config = {
       debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
   },
-  scene: [menu, Regles, selection, niveau1, niveau2, niveau3]
+  scene: [menu, Regles, selection, niveau1, niveau2, niveau3, dialogue]
 };
 
 // création et lancement du jeu
